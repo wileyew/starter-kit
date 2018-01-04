4af033f02c50f2b6ac2bcd2c8b5a20c9797c35ec
“Evan” <evanwiley@EVANs-MacBook-Pro.local>
2018-01-04 09:01:27 -0700

    fixed routing for the website.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './home/home.module';
+import { AboutModule } from './about/about.module';
+import { LoginModule } from './login/login.module';
 
 const routes: Routes = [
   // Fallback when no prior route is matched
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  { path: '', redirectTo: '', pathMatch: 'full', component: HomeModule },
+  { path: 'about', redirectTo: '', pathMatch: 'full', component: AboutModule },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule, HomeModule, AboutModule],
   providers: []
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { TranslateModule } from '@ngx-translate/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
-import { LoginModule } from './login/login.module';
-import { RouteModule, Routes } from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: 'home', component: HomeModule},
-  {path: 'about', component: AboutModule}
-]
+import { RouterModule, Routes } from '@angular/router';
 
 
 @NgModule({
@@ -30,9 +22,9 @@ const appRoutes: Routes = [
     NgbModule.forRoot(),
     CoreModule,
     SharedModule,
+    RouterModule,
     HomeModule,
     AboutModule,
-    LoginModule,
     AppRoutingModule
   ],
   declarations: [AppComponent],
