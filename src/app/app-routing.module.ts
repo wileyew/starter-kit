e3f050f3aa12b95fdb867a2bd90e11a8509d3f55
“Evan” <evanwiley@EVANs-MacBook-Pro.local>
2018-01-08 22:42:07 -0500

    adding routing for chapters module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
+import {ChaptersModule} from './chapters/chapters.module'
 import { AboutModule } from './about/about.module';
 import { LoginModule } from './login/login.module';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   // Fallback when no prior route is matched
   { path: '', redirectTo: '', pathMatch: 'full', component: HomeModule },
   { path: 'about', redirectTo: '', pathMatch: 'full', component: AboutModule },
+  {path: 'chapters', redirectTo: '', pathMatch: 'full', component: ChaptersModule}
 ];
 
 @NgModule({
---
+++ src/app/chapters/chapters.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { CoreModule } from '../core/core.module';
+import { SharedModule } from '../shared/shared.module';
+import { ChaptersRoutingModule } from './chapters-routing.module';
+import { ChaptersComponent } from './chapters.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TranslateModule,
+    CoreModule,
+    SharedModule,
+    ChaptersRoutingModule
+  ],
+  declarations: [
+    ChaptersComponent
+  ],
+  providers: [
+    QuoteService
+  ]
+})
+export class HomeModule { }
