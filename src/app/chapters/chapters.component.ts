8a58ac54e66a029277c4a7f1c82e72f89644f3f4
wileyew <edubme@gmail.com>
2018-03-04 21:50:48 -0500

    updated code with latest.

--- src/app/chapters/chapters.component.ts
+++ src/app/chapters/chapters.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
-
+import { EmbedVideoService } from 'ngx-embed-video';
 
 @Component({
   selector: 'app-chapters',
@@ -9,8 +9,22 @@ import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
 
 export class ChaptersComponent implements OnInit {
   decidingTwo: string = 'Types of Testing and Deciding on Test Automation framework for your team';
+  releaseDate: string = 'Determine release date for Potential Shippable Increments'
+  prioritizationOfFeatures: string = 'Prioritization of Features'
+  languageFrameworks: string = 'Determining the language framework for test automation'
+  mvp: string = 'Minimal Viable Product'
+  testDataCicd: string = 'Test Data Management and CICD'
+  perfTesting: string= 'Performance Testing the UI and API of your web application'
 
 @ViewChild("deciding", {read: ElementRef}) deciding: ElementRef;
+
+iframe_html: any;
+youtubeUrl = "https://youtu.be/3hHL-BhBW4I";
+
+constructor(private embedService: EmbedVideoService) {
+  console.log(this.embedService.embed(this.youtubeUrl));
+  this.iframe_html = this.embedService.embed(this.youtubeUrl);
+ }
 
   ngOnInit() { }
   scroll(element: string) {
